8ab0bea8079bc7e0ad174d1013ef98cfffe2c7da
Arturo <artuvntu@icloud.com>
2018-04-12 13:05:21 -0500

    Initial Commit

---
+++ CG2ProyectoFinalValencia/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG2ProyectoFinalValencia
+//
+//  Created by Arturo Ventura on 4/12/18.
+//  Copyright © 2018 vApps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
