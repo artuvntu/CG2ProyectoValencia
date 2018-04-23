9f247b70893443b3c5cc593b86fe7f99d90e6e8d
Arturo <artuvntu@icloud.com>
2018-04-23 00:32:51 -0500

    Restructuracion cargador imagen

--- CG2ProyectoFinalValencia/Source/CargadorImage/CargadorImage.cpp
+++ CG2ProyectoFinalValencia/Source/CargadorImage/CargadorImage.cpp
@@ -8,7 +8,19 @@
 
 #include "CargadorImage.hpp"
 
-int CargadorImage::newTypeTexture(int where){
+unsigned int CargadorImage::newTypeTexture(){
+    int where = (int)this->glindexs.size();
+    this->types.push_back(where);
+    return (unsigned int)this->types.size() - 1;
+}
+bool CargadorImage::newTexture(std::string path){
     
-    return where;
+    
+}
+bool CargadorImage::setDefaultTypeTexture(unsigned int type){
+    if (type < this->types.size()){
+        this->defaultTypeTexture = type;
+        return true;
+    }
+    return false;
 }
--- CG2ProyectoFinalValencia/Source/CargadorImage/CargadorImage.hpp
+++ CG2ProyectoFinalValencia/Source/CargadorImage/CargadorImage.hpp
@@ -9,11 +9,12 @@
 
 class CargadorImage{
 public:
+    unsigned int newTypeTexture();
+    bool newTexture(std::string path);
+    bool setDefaultTypeTexture(unsigned int type);
     unsigned int getGLIndexForElement(int element,int type = -1);
-    int newTypeTexture(int where = -1);
-    int setDefaultTypeTexture(int type);
-    bool newTexture(std::string path,int type=-1);
 private:
     std::vector<unsigned int> glindexs;
     std::vector<int> types;
+    int defaultTypeTexture = -1;
 };
